c2e5ad4ec5797d9544bcf0fb563b68ba9f316b90
Xavier Crochet <xavier@futurae.com>
2025-01-10 12:04:39 +0100

    Integrate RequestContext middleware in webapp

--- web/app.go
+++ web/app.go
@@ -75,47 +75,49 @@ func SetupRoutes(ctx context.Context, router *http.ServeMux, options *ServerOpti
 
 	   if the request is successfull, the user is redirected to /feed
 	*/
-	router.Handle("/select_feed", mw.LogMiddleware(authMw.RequireAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		if req.Method != http.MethodPost {
-			http.Error(w, "not found", http.StatusNotFound)
-			return
-		}
-		authCtx := authMw.Context(req.Context())
+	router.Handle("/select_feed",
+		mw.RequestContextMiddleware(
+			mw.LogMiddleware(authMw.RequireAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				if req.Method != http.MethodPost {
+					http.Error(w, "not found", http.StatusNotFound)
+					return
+				}
+				authCtx := authMw.Context(req.Context())
 
-		// deserialize request payload
-		err := req.ParseForm()
-		if err != nil {
-			http.Error(w, "failed to parse form data", http.StatusBadRequest)
-			return
-		}
+				// deserialize request payload
+				err := req.ParseForm()
+				if err != nil {
+					http.Error(w, "failed to parse form data", http.StatusBadRequest)
+					return
+				}
 
-		// validate user input
-		name := req.FormValue("name")
-		if name == "" {
-			http.Error(w, "name can't be empty", http.StatusBadRequest)
-		}
-		// sanitize user input
-		name = html.EscapeString(name)
+				// validate user input
+				name := req.FormValue("name")
+				if name == "" {
+					http.Error(w, "name can't be empty", http.StatusBadRequest)
+				}
+				// sanitize user input
+				name = html.EscapeString(name)
 
-		// http client that integrate the feed api
-		feedClient := feed_api.NewFeedClient(options.apiHostname, options.apiPort)
-		if err := feedClient.SelectFeed(name, authCtx.Tokens.AccessToken); err == net.ErrNoAccess {
-			slog.Error("select feed api call failed", "error", err)
-			http.Error(w, err.Error(), http.StatusForbidden)
-			return
-		} else if err == net.ErrNotAuthenticated {
-			slog.Error("select feed api call failed", "error", err)
-			http.Error(w, err.Error(), http.StatusUnauthorized)
-			return
-		} else if err != nil {
-			slog.Error("select feed api call failed", "error", err)
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+				// http client that integrate the feed api
+				feedClient := feed_api.NewFeedClient(options.apiHostname, options.apiPort)
+				if err := feedClient.SelectFeed(name, authCtx.Tokens.AccessToken); err == net.ErrNoAccess {
+					slog.Error("select feed api call failed", "error", err)
+					http.Error(w, err.Error(), http.StatusUnauthorized)
+					return
+				} else if err == net.ErrNotAuthenticated {
+					slog.Error("select feed api call failed", "error", err)
+					http.Error(w, err.Error(), http.StatusForbidden)
+					return
+				} else if err != nil {
+					slog.Error("select feed api call failed", "error", err)
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
 
-		// browser expect a http status 3XX if we want to redirect after a successfull post
-		http.Redirect(w, req, "/feed", http.StatusSeeOther)
-	}))))
+				// browser expect a http status 3XX if we want to redirect after a successfull post
+				http.Redirect(w, req, "/feed", http.StatusSeeOther)
+			})))))
 
 	/*
 	   This endpoint
@@ -127,61 +129,62 @@ func SetupRoutes(ctx context.Context, router *http.ServeMux, options *ServerOpti
 
 	*/
 
-	router.Handle("/feed", mw.LogMiddleware(authMw.RequireAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		// Using the [middleware.Context] function we can gather information about the authenticated user.
-		// This example will just print a JSON representation of the UserInfo of the typed [*oidc.UserInfoContext].
-		authCtx := authMw.Context(req.Context())
-		feedPage := NewFeedPage(authCtx.UserInfo.GivenName, authCtx.UserInfo.FamilyName)
+	router.Handle("/feed",
+		mw.RequestContextMiddleware(
+			mw.LogMiddleware(authMw.RequireAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+				// Using the [middleware.Context] function we can gather information about the authenticated user.
+				// This example will just print a JSON representation of the UserInfo of the typed [*oidc.UserInfoContext].
+				authCtx := authMw.Context(req.Context())
+				feedPage := NewFeedPage(authCtx.UserInfo.GivenName, authCtx.UserInfo.FamilyName)
 
-		/*
-		  check if health API is healthy
-		  ideally, this should be part of a middleware
-		*/
+				/*
+				  check if health API is healthy
+				  ideally, this should be part of a middleware
+				*/
 
-		feedClient := feed_api.NewFeedClient(options.apiHostname, options.apiPort)
-		if ok, err := feedClient.CheckHealth(); !ok {
-			feedPage.Health = false
+				feedClient := feed_api.NewFeedClient(options.apiHostname, options.apiPort)
+				if ok, err := feedClient.CheckHealth(); !ok {
+					feedPage.Health = false
 
-			if err != nil {
-				slog.Error("feed api is down or unresponsive", "error", err)
-			}
-		} else {
-			// only query for feed if feed API is healthy
-			feed, err := feedClient.GetFeed(authCtx.Tokens.AccessToken)
-			// if the token is not valid anymore, log out the user
-			if err == net.ErrNotAuthenticated {
-				http.Redirect(w, req, "/auth/logout", http.StatusFound)
-				return
-			} else if err != nil {
-				slog.Error("feed api call failed", "error", err)
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
+					if err != nil {
+						slog.Error("feed api is down or unresponsive", "error", err)
+					}
+				} else {
+					// only query for feed if feed API is healthy
+					feed, err := feedClient.GetFeed(authCtx.Tokens.AccessToken)
+					if err != nil {
+						slog.Error("feed api call failed", "error", err)
+						http.Error(w, err.Error(), http.StatusInternalServerError)
+						return
+					}
 
-			feedPage.Feed = feed
-		}
+					feedPage.Feed = feed
+				}
 
-		err = t.ExecuteTemplate(w, "feed.html", feedPage)
-		if err != nil {
-			slog.Error("error writing feed response", "error", err)
-		}
-	}))))
+				err = t.ExecuteTemplate(w, "feed.html", feedPage)
+				if err != nil {
+					slog.Error("error writing feed response", "error", err)
+				}
+			})))))
 
 	// This endpoint is accessible by anyone, but it will check if there already is a valid session (authentication).
 	// If there is an active session, the information will be put into the context for later retrieval.
-	router.Handle("/", mw.LogMiddleware(authMw.CheckAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+	router.Handle("/",
+		mw.RequestContextMiddleware(
+			mw.LogMiddleware(authMw.CheckAuthentication()(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 
-		// redirect the user to /feed in case he is already authenticated
-		if authentication.IsAuthenticated(req.Context()) {
-			http.Redirect(w, req, "/feed", http.StatusFound)
-			return
-		}
+				// redirect the user to /feed in case he is already authenticated
+				if authentication.IsAuthenticated(req.Context()) {
+					http.Redirect(w, req, "/feed", http.StatusFound)
+					return
+				}
 
-		err = t.ExecuteTemplate(w, "home.html", nil)
-		if err != nil {
-			slog.Error("error writing home page response", "error", err)
-		}
-	}))))
+				err = t.ExecuteTemplate(w, "home.html", nil)
+				if err != nil {
+					slog.Error("error writing home page response", "error", err)
+				}
+			})))))
+
 	return nil
 }
 
