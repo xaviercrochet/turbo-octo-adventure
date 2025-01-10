e1b1d580ef76ff422d2504f0de8d446bb38d5a10
Xavier Crochet <xavier@futurae.com>
2025-01-10 12:04:23 +0100

    Integrate RequestContext middleware in api

--- api/app/app.go
+++ api/app/app.go
@@ -72,13 +72,15 @@ func SetupRoutes(ctx context.Context, router *http.ServeMux, options *ServerOpti
 	authMw := middleware.New(authZ)
 
 	// This endpoint is accessible by anyone and will always return "200 OK" to indicate the API is running
-	router.Handle("/api/healthz", mw.LogMiddleware(http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			err = jsonResponse(w, "OK", http.StatusOK)
-			if err != nil {
-				slog.Error("error writing response", "error", err)
-			}
-		})))
+	router.Handle("/api/healthz",
+		mw.RequestContextMiddleware(
+			mw.LogMiddleware(http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					err = jsonResponse(w, "OK", http.StatusOK)
+					if err != nil {
+						slog.Error("error writing response", "error", err)
+					}
+				}))))
 
 	/*
 
@@ -95,47 +97,49 @@ func SetupRoutes(ctx context.Context, router *http.ServeMux, options *ServerOpti
 	   - 404 if http verb is not POST or username doesn't exist
 	*/
 
-	router.Handle("/api/select_feed", mw.LogMiddleware(authMw.RequireAuthorization()(http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			if r.Method != http.MethodPost {
-				http.Error(w, "not found", http.StatusNotFound)
-				return
-			}
+	router.Handle("/api/select_feed", mw.RequestContextMiddleware(
+		mw.LogMiddleware(
+			authMw.RequireAuthorization()(http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					if r.Method != http.MethodPost {
+						http.Error(w, "not found", http.StatusNotFound)
+						return
+					}
 
-			authCtx := authMw.Context(r.Context())
-			if !authCtx.IsGrantedRole("admin") {
-				slog.Warn("user doesn't have access to the resource", "id", authCtx.UserID(), "username", authCtx.Username)
-				http.Error(w, "forbidden", http.StatusForbidden)
-				return
-			}
+					authCtx := authMw.Context(r.Context())
+					if !authCtx.IsGrantedRole("admin") {
+						slog.Warn("user doesn't have access to the resource", "id", authCtx.UserID(), "username", authCtx.Username)
+						http.Error(w, "forbidden", http.StatusForbidden)
+						return
+					}
 
-			// deserialize the request payload
-			body, err := io.ReadAll(r.Body)
-			if err != nil {
-				slog.Warn("could not read request body", "id", authCtx.UserID(), "username", authCtx.Username, "error", err)
-				http.Error(w, "Error reading request body", http.StatusBadRequest)
-				return
-			}
-			defer r.Body.Close()
+					// deserialize the request payload
+					body, err := io.ReadAll(r.Body)
+					if err != nil {
+						slog.Warn("could not read request body", "id", authCtx.UserID(), "username", authCtx.Username, "error", err)
+						http.Error(w, "Error reading request body", http.StatusBadRequest)
+						return
+					}
+					defer r.Body.Close()
 
-			var selectedFeed SelectedFeed
-			err = json.Unmarshal(body, &selectedFeed)
-			if err != nil {
-				slog.Warn("could not deserialize request body", "id", authCtx.UserID(), "username", authCtx.Username, "error", err)
-				http.Error(w, "failed to deserialize request body", http.StatusBadRequest)
-				return
-			}
+					var selectedFeed SelectedFeed
+					err = json.Unmarshal(body, &selectedFeed)
+					if err != nil {
+						slog.Warn("could not deserialize request body", "id", authCtx.UserID(), "username", authCtx.Username, "error", err)
+						http.Error(w, "failed to deserialize request body", http.StatusBadRequest)
+						return
+					}
 
-			// update the username from wich '/feed' will retrieve the musicbrainz feed from
+					// update the username from wich '/feed' will retrieve the musicbrainz feed from
 
-			setSelectedUsername(selectedFeed.Name)
+					setSelectedUsername(selectedFeed.Name)
 
-			// OK
-			err = jsonResponse(w, "OK", http.StatusOK)
-			if err != nil {
-				slog.Error("error writing response", "error", err)
-			}
-		}))))
+					// OK
+					err = jsonResponse(w, "OK", http.StatusOK)
+					if err != nil {
+						slog.Error("error writing response", "error", err)
+					}
+				})))))
 
 	/*
 	   Retrieve music feed from feed API, based on selectedUsername
@@ -148,40 +152,42 @@ func SetupRoutes(ctx context.Context, router *http.ServeMux, options *ServerOpti
 	   - 404 if http verb is not GET
 	*/
 
-	router.Handle("/api/feed", mw.LogMiddleware(authMw.RequireAuthorization()(http.HandlerFunc(
-		func(w http.ResponseWriter, r *http.Request) {
-			if r.Method != http.MethodGet {
-				http.Error(w, "not found", http.StatusNotFound)
-				return
-			}
+	router.Handle("/api/feed",
+		mw.RequestContextMiddleware(
+			mw.LogMiddleware(authMw.RequireAuthorization()(http.HandlerFunc(
+				func(w http.ResponseWriter, r *http.Request) {
+					if r.Method != http.MethodGet {
+						http.Error(w, "not found", http.StatusNotFound)
+						return
+					}
 
-			authCtx := authMw.Context(r.Context())
-			slog.Info("retrieving user feed", "id", authCtx.UserID(), "username", authCtx.Username, "feed_username", getSelectedUsername())
+					authCtx := authMw.Context(r.Context())
+					slog.Info("retrieving user feed", "id", authCtx.UserID(), "username", authCtx.Username, "feed_username", getSelectedUsername())
 
-			// retrieve music feed from musicbrainz API
-			feed, err := musicbrainz.GetFeed(getSelectedUsername())
+					// retrieve music feed from musicbrainz API
+					feed, err := musicbrainz.GetFeed(getSelectedUsername())
 
-			// handle client error if any
-			if err == net.ErrNotFound {
-				http.Error(w, "feed not found", http.StatusNotFound)
-			} else if err != nil {
-				slog.Warn("musicbrainz api call failed", "error", err)
-				http.Error(w, "musicbrainz api call failed", http.StatusInternalServerError)
-			}
+					// handle client error if any
+					if err == net.ErrNotFound {
+						http.Error(w, "feed not found", http.StatusNotFound)
+					} else if err != nil {
+						slog.Warn("musicbrainz api call failed", "error", err)
+						http.Error(w, "musicbrainz api call failed", http.StatusInternalServerError)
+					}
 
-			/*
-			   Return the feed and weather the user has sufficiant authorization to update selectedUsername
-			*/
-			resp := &FeedResponse{
-				Feed:        feed,
-				WriteAccess: authCtx.IsGrantedRole("admin"),
-			}
+					/*
+					   Return the feed and weather the user has sufficiant authorization to update selectedUsername
+					*/
+					resp := &FeedResponse{
+						Feed:        feed,
+						WriteAccess: authCtx.IsGrantedRole("admin"),
+					}
 
-			err = jsonResponse(w, resp, http.StatusOK)
-			if err != nil {
-				slog.Error("error writing response", "error", err)
-			}
-		}))))
+					err = jsonResponse(w, resp, http.StatusOK)
+					if err != nil {
+						slog.Error("error writing response", "error", err)
+					}
+				})))))
 
 	return nil
 }
